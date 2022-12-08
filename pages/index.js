fddb9eda13b9a675261b6a91abe1c083293f47f1
daniel ikokoh <danielikokoh@MACs-MacBook-Pro.local>
2022-12-08 18:45:26 +0100

    fix: rename components Components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import WhyKaya from '../components/Landing/WhyKaya'
 import KayaCapital from '../components/Landing/KayaCapital'
 
