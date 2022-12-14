3a84219706a96018fc86069f2bea8020596051c7
daniel ikokoh <danielikokoh@MACs-MBP.ipnxng.net>
2022-12-14 14:34:24 +0100

    fix: fixed alignment issue on the about us section

--- Components/Landing/AboutKaya.jsx
+++ Components/Landing/AboutKaya.jsx
@@ -4,10 +4,10 @@ import SubHeader from "../Common/SubHeader";
 
 function AboutKaya() {
   return (
-    <section className="py-14 bg-lightGray lg:my-5">
+    <section className="py-14 bg-lightGray lg:my-5 duration-500 transition-all ease-in-out">
       <div className="container px-4 mx-auto">
-        <div className="flex flex-col items-center justify-between space-y-16 lg:flex-row">
-          <div className="flex-1">
+        <div className="grid grid-cols-1 gap-y-10 lg:place-items-center lg:grid-cols-2 lg:gap-y-0 lg:gap-x-10">
+          <div className="w-full">
             <Image
               src="/img-placeholder.png"
               width={580}
@@ -15,7 +15,7 @@ function AboutKaya() {
               alt="decorative image"
             />
           </div>
-          <div className="flex-1">
+          <div className="w-full">
             <SubHeader title="KAYA"/>
             <h1 className="mb-10 text-4xl font-bold text-gray-900">About Kayapay</h1>
             <p className="text-base leading-snug mb-7">Lorem ipsum dolor sit amet consectetur. Nam varius lacus lacus suspendisse vitae. Vitae nunc eget tristique malesuada nunc cum aliquam interdum. Fames ut pellentesque semper pellentesque etiam. Sed dapibus facilisis nisi suspendisse porta.</p>
