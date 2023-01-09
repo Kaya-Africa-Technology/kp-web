69152007b421cb4bfd9bab91a240864fde0154ec
daniel ikokoh <danielikokoh@MACs-MBP.ipnxng.net>
2023-01-09 08:58:17 +0100

    fix: fixed the sponsor section structure

--- Components/Landing/Sponsor.jsx
+++ Components/Landing/Sponsor.jsx
@@ -3,17 +3,19 @@ import SubHeader from "../Common/SubHeader";
 
 function Sponsor() {
   return (
-    <section className="container px-4 mx-auto my-24 lg:my-20">
+    <section className="my-24 section-container lg:my-20">
       <SubHeader title="KAYA" />
       <h1 className="mb-10 text-4xl font-bold text-gray-900">Our Sponsor</h1>
-      <div className="grid grid-cols-2 gap-5 lg:grid-cols-8">
-        <img src="/brand.svg" alt="" width="200" height="30" />
-        <img src="/brand.svg" alt="" width="200" height="30" />
-        <img src="/brand.svg" alt="" width="200" height="30" />
-        <img src="/brand.svg" alt="" width="200" height="30" />
-        <img src="/brand.svg" alt="" width="200" height="30" />
-        <img src="/brand.svg" alt="" width="200" height="30" />
-        <img src="/brand.svg" alt="" width="200" height="30" />
+      <div className="flex items-center justify-center">
+         <div className="grid grid-cols-2 gap-5 lg:grid-cols-8">
+          <img src="/brand.svg" alt="" width="200" height="30" />
+          <img src="/brand.svg" alt="" width="200" height="30" />
+          <img src="/brand.svg" alt="" width="200" height="30" />
+          <img src="/brand.svg" alt="" width="200" height="30" />
+          <img src="/brand.svg" alt="" width="200" height="30" />
+          <img src="/brand.svg" alt="" width="200" height="30" />
+          <img src="/brand.svg" alt="" width="200" height="30" />
+        </div>
       </div>
     </section>
   );
